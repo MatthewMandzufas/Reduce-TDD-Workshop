daf1aa10b0ce06fdb1035c00228f8c46e8b1db84
Matthew Mandzufas <matthew.mandzufas@journeyone.com.au>
2024-02-21 15:40:31 +0800

    Green: Reduce Should use the initial value as the accumulator instead of the first element of the array

--- src/reduce.js
+++ src/reduce.js
@@ -1,11 +1,24 @@
 
 
-function reduce(arrayToReduce, reducer){
-    let accumulator = arrayToReduce[0];
-    for(let i = 1; i < arrayToReduce.length; i++){
+function reduce(arrayToReduce, reducer, initialValue){
+    let accumulator;
+    let startIndex;
+    if(initialValue === undefined){
+        accumulator = arrayToReduce[0];
+        startIndex = 1;
+        
+
+    } else{
+        accumulator = initialValue; 
+        startIndex = 0;
+    }
+
+    for(let i = startIndex; i < arrayToReduce.length; i++){
         accumulator = reducer(accumulator, arrayToReduce[i])
     }
+
     return accumulator;
+    
 }
 
 export default reduce;
