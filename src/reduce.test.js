10a84c6e886a87e0baa23223127d66a5b109fde3
Matthew Mandzufas <matthew.mandzufas@journeyone.com.au>
2024-02-21 15:19:54 +0800

    Red: Reduce › Should use the initial value as the accumulator instead of the first element of the array

--- src/reduce.test.js
+++ src/reduce.test.js
@@ -5,11 +5,21 @@ import reduce from "./reduce"
 describe('Reduce', () => {
     it('Should reduce an array of numbers to their sum', ()=> {
         const arrayToReduce = [1, 2, 3, 4];
-        function reducer(accumuator, currentItem) {
-            return accumuator + currentItem;
+        function reducer(accumulator, currentItem) {
+            return accumulator + currentItem;
         }
 
         const sumOfNumbersInAnArray = reduce(arrayToReduce, reducer);
         expect(sumOfNumbersInAnArray).toBe(10);
+    }),
+    it('Should use the initial value as the accumulator instead of the first element of the array', ()=> {
+        const arrayToReduce = [1, 2, 3, 4];
+        const initialValue = 4;
+        function reducer(accumulator, currentItem) {
+            return accumulator + currentItem;
+        }
+
+        const sumOfNumbersInAnArray = reduce(arrayToReduce, reducer, initialValue);
+        expect(sumOfNumbersInAnArray).toBe(14);
     })
 })
