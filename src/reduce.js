91d09b05b0600e480367c8438fcd6119269d9a07
Matthew Mandzufas <matthew.mandzufas@journeyone.com.au>
2024-03-06 13:00:15 +0800

    RED: Reduce › Should only operate on an array of numbers

--- src/reduce.js
+++ src/reduce.js
@@ -1,26 +1,24 @@
-
-
-function reduce(arrayToReduce, reducer, initialValue){
-    let accumulator;
-    let startIndex;
-    if(initialValue === undefined){
-        if(arrayToReduce.length === 0){
-            throw new Error("An initial value must be provided when reducing an empty array")
-        }
-        accumulator = arrayToReduce[0];
-        startIndex = 1;
-     
-    } else{
-        accumulator = initialValue; 
-        startIndex = 0;
+function reduce(arrayToReduce, reducer, initialValue) {
+  let accumulator;
+  let startIndex;
+  if (initialValue === undefined) {
+    if (arrayToReduce.length === 0) {
+      throw new Error(
+        "An initial value must be provided when reducing an empty array",
+      );
     }
+    accumulator = arrayToReduce[0];
+    startIndex = 1;
+  } else {
+    accumulator = initialValue;
+    startIndex = 0;
+  }
 
-    for(let i = startIndex; i < arrayToReduce.length; i++){
-        accumulator = reducer(accumulator, arrayToReduce[i])
-    }
+  for (let i = startIndex; i < arrayToReduce.length; i++) {
+    accumulator = reducer(accumulator, arrayToReduce[i]);
+  }
 
-    return accumulator;
-    
+  return accumulator;
 }
 
 export default reduce;
--- src/reduce.test.js
+++ src/reduce.test.js
@@ -1,81 +1,92 @@
-import reduce from "./reduce"
+import reduce from "./reduce";
 
+describe("Reduce", () => {
+  it("Should reduce an array of numbers to their sum", () => {
+    const arrayToReduce = [1, 2, 3, 4];
+    function reducer(accumulator, currentItem) {
+      return accumulator + currentItem;
+    }
 
+    const sumOfNumbersInAnArray = reduce(arrayToReduce, reducer);
+    expect(sumOfNumbersInAnArray).toBe(10);
+  }),
+    it("Should use the initial value as the accumulator instead of the first element of the array", () => {
+      const arrayToReduce = [1, 2, 3, 4];
+      const initialValue = 4;
+      function reducer(accumulator, currentItem) {
+        return accumulator + currentItem;
+      }
 
-describe('Reduce', () => {
-    it('Should reduce an array of numbers to their sum', ()=> {
-        const arrayToReduce = [1, 2, 3, 4];
-        function reducer(accumulator, currentItem) {
-            return accumulator + currentItem;
-        }
-
-        const sumOfNumbersInAnArray = reduce(arrayToReduce, reducer);
-        expect(sumOfNumbersInAnArray).toBe(10);
+      const sumOfNumbersInAnArray = reduce(
+        arrayToReduce,
+        reducer,
+        initialValue,
+      );
+      expect(sumOfNumbersInAnArray).toBe(14);
     }),
-    it('Should use the initial value as the accumulator instead of the first element of the array', ()=> {
-        const arrayToReduce = [1, 2, 3, 4];
+    it("Should not modify the original array", () => {
+      const array = [1, 2, 3, 4];
+      const initialValue = 4;
+      function reducer(accumulator, currentItem) {
+        return accumulator + currentItem;
+      }
+
+      reduce(array, reducer, initialValue);
+      expect(array).toStrictEqual([1, 2, 3, 4]);
+    }),
+    it("Should only operate on an array of numbers", () => {
+      const arrayToReduce = [1, 2, undefined, 5, 6];
+      function reducer(accumulator, currentItem) {
+        return accumulator + currentItem;
+      }
+      expect(() => reduce(arrayToReduce, reducer)).toThrow(
+        "Array contains non-numbers",
+      );
+    });
+  describe("Given an initial value", () => {
+    it("Calls the reducer once per element in the array", () => {
+      const arrayToReduce = [1, 2, 3, 4];
+      const mockReducer = jest.fn();
+      const numberOfItemsInArray = arrayToReduce.length;
+      const initialValue = 4;
+
+      reduce(arrayToReduce, mockReducer, initialValue);
+      expect(mockReducer).toHaveBeenCalledTimes(numberOfItemsInArray);
+    });
+    describe("and an empty array", () => {
+      it("returns the initial value", () => {
+        const arrayToReduce = [];
         const initialValue = 4;
         function reducer(accumulator, currentItem) {
-            return accumulator + currentItem;
+          return accumulator + currentItem;
         }
 
-        const sumOfNumbersInAnArray = reduce(arrayToReduce, reducer, initialValue);
-        expect(sumOfNumbersInAnArray).toBe(14);
-    }),
-    it('Should not modify the original array', ()=> {
-        const array = [1, 2, 3, 4];
-        const initialValue = 4;
-        function reducer(accumulator, currentItem){
-            return accumulator + currentItem;
+        const reducedValue = reduce(arrayToReduce, reducer, initialValue);
+        expect(reducedValue).toEqual(initialValue);
+      });
+    });
+  });
+
+  describe("Given no initial value", () => {
+    it("calls the reducer n - 1 times for an array with n elements", () => {
+      const mockReducer = jest.fn();
+      const arrayToReduce = [1, 2, 3, 4];
+      const numberOfItemsInArray = arrayToReduce.length;
+      reduce(arrayToReduce, mockReducer);
+
+      expect(mockReducer).toHaveBeenCalledTimes(numberOfItemsInArray - 1);
+    });
+    describe("and an empty array", () => {
+      it("throws an error", () => {
+        const array = [];
+        function reducer(accumulator, currentItem) {
+          return accumulator + currentItem;
         }
-        
-        reduce(array, reducer, initialValue);
-        expect(array).toStrictEqual([1, 2, 3, 4]);
-    })
-    describe('Given an initial value', ()=> {
-        it('Calls the reducer once per element in the array', ()=> {
-            const arrayToReduce = [1, 2, 3, 4];
-            const mockReducer = jest.fn();
-            const numberOfItemsInArray = arrayToReduce.length;
-            const initialValue = 4;
-            
-            reduce(arrayToReduce, mockReducer, initialValue);
-            expect(mockReducer).toHaveBeenCalledTimes(numberOfItemsInArray);
-        })
-        describe('and an empty array', ()=>{
-            it('returns the initial value', ()=> {
-                const arrayToReduce = [];
-                const initialValue = 4;
-                function reducer(accumulator, currentItem){
-                    return accumulator + currentItem;
-                }
 
-                const reducedValue = reduce(arrayToReduce, reducer, initialValue);
-                expect(reducedValue).toEqual(initialValue);
-
-            })
-        })
-    })
-
-    describe('Given no initial value', ()=>{
-        it('calls the reducer n - 1 times for an array with n elements', ()=>{
-            const mockReducer = jest.fn();
-            const arrayToReduce = [1, 2, 3, 4];
-            const numberOfItemsInArray = arrayToReduce.length;
-            reduce(arrayToReduce, mockReducer);
-           
-            expect(mockReducer).toHaveBeenCalledTimes(numberOfItemsInArray - 1);
-        })
-        describe('and an empty array', ()=>{
-            it('throws an error', ()=> {
-                const array = [];
-                function reducer(accumulator, currentItem){
-                    return accumulator + currentItem;
-                }
-
-                expect(() => reduce(array, reducer)).toThrow("An initial value must be provided when reducing an empty array");
-            
-            })
-        })
-    })
-})
+        expect(() => reduce(array, reducer)).toThrow(
+          "An initial value must be provided when reducing an empty array",
+        );
+      });
+    });
+  });
+});
